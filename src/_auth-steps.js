789ff2926210b77adf1f49caf5bbdf4501dbe98a
alizubairr <axubair9@gmail .com>
2022-10-16 15:21:21 +0600

    Firebase intialize complete

---
+++ src/_auth-steps.js
@@ -0,0 +1,15 @@
+/* 
+    1. Create a new project in console.firebase.google.com
+    2. npm install firebase
+    3. Create a initialise file in 'src' and name will be firebase.init.js
+    4. Export app and getAuth(app)
+    5. Go to fire settings then enable email/password auth
+*/
+
+
+/* 
+    // Hosting Step
+    1. npm install -g firebase-tools
+    2. firebase login, firebase init, firebase deploy
+    3. 
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAfml7JsbhyCzUaYuCRzfMmhxLB6wzDeT8",
+    authDomain: "ema-jhon-ecommerce-shop.firebaseapp.com",
+    projectId: "ema-jhon-ecommerce-shop",
+    storageBucket: "ema-jhon-ecommerce-shop.appspot.com",
+    messagingSenderId: "68824439819",
+    appId: "1:68824439819:web:744014e24b0110964dfb0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export default app;
